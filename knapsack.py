f8112ee952058e9e95d936bdf82b803266208f8e
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-24 20:28:14 -0300

    criação do método ordenar_Vu

--- knapsack.py
+++ knapsack.py
@@ -1,7 +1,9 @@
+from operator import itemgetter
+
 class Mochila:
     def __init__(self, peso_maximo: float) -> None:
         self.peso_maximo = peso_maximo
-        self.items = dict()
+        self.itens = dict()
         self.peso_ocupado = 0
 
     def get_peso_max(self):
@@ -10,13 +12,20 @@ class Mochila:
     def set_peso_max(self, x):
         self.peso_maximo = x
 
+    def get_peso_ocupado(self):
+        return self.peso_ocupado
+    
+    def get_itens(self):
+        return self.itens
+
 
 
 class item:
-    def __init__(self, etiqueta, peso, preco):
-        self.etiqueta = etiqueta
-        self.peso_item = peso
-        self.valor_item = preco
+    def __init__(self, etiqueta, peso, valor):
+        self.itens = dict()
+        self.itens[etiqueta] = [peso, valor]
+        self.Vu = dict()    # Vu = valor utilitário (valor/peso)
+        self.Vu[etiqueta] = valor/peso
 
     def get_preco_item(self):
         return self.valor_item
@@ -26,4 +35,15 @@ class item:
 
     def get_etiqueta(self):
         return self.etiqueta
+    
+    def ordenar_Vu(self):
+        return sorted(self.Vu.items(), key=itemgetter(1))
+    
+    
+    
+    
+    
+
+
+
     
