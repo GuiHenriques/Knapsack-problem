1157635df9098d6487a9d161a4b8944cb7043fa6
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-24 21:02:34 -0300

    correções na classe knapsack

--- knapsack.py
+++ knapsack.py
@@ -3,8 +3,9 @@ from operator import itemgetter
 class Mochila:
     def __init__(self, peso_maximo: float) -> None:
         self.peso_maximo = peso_maximo
-        self.itens = dict()
         self.peso_ocupado = 0
+        self.itens = list()
+        self.valor = 0
 
     def get_peso_max(self):
         return self.peso_maximo
@@ -17,15 +18,15 @@ class Mochila:
     
     def get_itens(self):
         return self.itens
-
+    
+    def get_valor(self):
+        return self.valor
 
 
 class item:
-    def __init__(self, etiqueta, peso, valor):
-        self.itens = dict()
-        self.itens[etiqueta] = [peso, valor]
-        self.Vu = dict()    # Vu = valor utilitário (valor/peso)
-        self.Vu[etiqueta] = valor/peso
+    def __init__(self):
+        self.itens = dict()       
+        self.Vu = dict()    # Vu = valor utilitário (valor/peso)        
 
     def get_preco_item(self):
         return self.valor_item
@@ -36,10 +37,19 @@ class item:
     def get_etiqueta(self):
         return self.etiqueta
     
+    def get_itens(self):
+        return self.itens
+    
+    def get_Vu(self):
+        return self.Vu
+    
     def ordenar_Vu(self):
-        return sorted(self.Vu.items(), key=itemgetter(1))
+        return sorted(self.Vu.items(), key=itemgetter(1)) # ordena do menor pro maior
     
-    
+    def add_item(self, etiqueta, peso, valor):
+        self.itens[etiqueta] = [peso, valor]
+        self.Vu[etiqueta] = valor/peso
+
     
     
     
