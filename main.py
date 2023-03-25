bfa8091961c9787eb8205f7eac27dd839e76dbfb
Erik Orsolin <depaulaerik670@gmail.com>
2023-03-25 19:36:54 -0300

    caso de teste

--- main.py
+++ main.py
@@ -1,17 +1,35 @@
 from knapsack import *
-
-# n = int(input('Digite a quantidade de itens: '))
-# p = float(input('Digite o peso máximo da mochila: '))
     
+# PESO, VALOR, ETIQUETA
 itens = [
     Item(10, 10, "A"),
     Item(20, 50, "B"),
     Item(40, 30, "C"),
     Item(40, 50, "D"),
-    Item(70, 95, "E")
+    Item(70, 95, "E"),
+    Item(30, 56, 'F'),
+    Item(40, 67, 'G'),
+    Item(45, 87, 'H'),
+    Item(67, 89, 'J'),
+    Item(54, 80, 'K'),
+    Item(89, 90, 'L'),
+    Item(45, 85, 'M'),
+    Item(50, 70, 'N'),
+    Item(60, 80, 'O'),
+    Item(56, 78, 'P'),
+    Item(45, 80, 'Q'),
+    Item(70, 90, 'R'),
+    Item(890, 60, 'S'),
+    Item(70, 210, 'T'),
+    Item(110, 70, 'U'),
+    Item(122, 459, 'V'),
+    Item(80, 20, 'W'),
+    Item(50, 110, 'X'),
+    Item(10, 60, 'Y'),
+    Item(70, 770, 'z'), 
 ]
 
-mochila = Mochila(100)
+mochila = Mochila(300)
 
 manejador = ManejadorItem()
 for item in itens:
